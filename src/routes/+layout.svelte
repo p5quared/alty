1f4111666aaab6bb6ae96dd67a3d46eaf9ffbb73
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-11-04 17:07:21 -0400

    creation toasts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,12 @@
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	import { storePopup, Toast, initializeStores } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	initializeStores();
 </script>
+
+	<Toast />
 
 <div class="pt-8">
 	<slot />
