10a00c9eca2a4731e571eef4b666025490dbe6db
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-11-07 22:05:02 -0500

    og_preview

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -21,14 +21,31 @@
 		}
 		toastStore.trigger(t);
 	}
+
+	let og_title = "Cat Facts";
+	let og_image = "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg";
+	let og_descr = "Cat facts are the best facts!";
 </script>
 
 <div class="container mx-auto space-y-5">
 	<h1 class="h1">Create a Misdirection!</h1>
-	<form  method="POST" class="space-y-7 lg:max-w-lg">
+	<div class="lg:flex lg:gap-14">
+		<div class="order-2">
+		<h2 class="h2">Opengraph Preview<h2>
+		<div class="card variant-filled-surface overflow-hidden max-w-lg h-min mt-4">
+			<header>
+			<img src={og_image} alt={og_title} class="w-full max-h-96 object-cover">
+			</header>
+			<div class="p-4 space-y-2">
+				<h2 class="h2 text-primary-400">{og_title}</h2>
+				<p class="text-sm font-sans">{og_descr}</p>
+			</div>
+		</div>
+	</div>
+	<form  method="POST" class="space-y-7 w-full max-w-lg order-1">
 		<label for="title">
 		<span class="text-lg">Title</span>
-			<input type="text" name="og_title" id="title" class="form-input mt-1 block w-full outline outline-2 outline-primary-500 text-black" placeholder="Misdirection Title">
+			<input bind:value={og_title} type="text" name="og_title" id="title" class="form-input mt-1 block w-full outline outline-2 outline-primary-500 text-black" placeholder="Misdirection Title">
 		</label>
 
 		<label for="type">
@@ -43,7 +60,7 @@
 
 		<label for="image">
 		<span class="text-lg">Image URL</span>
-			<input type="url" name="og_image" id="image" class="form-input mt-1 block w-full outline outline-2 outline-primary-500 text-black"
+			<input bind:value={og_image} type="url" name="og_image" id="image" class="form-input mt-1 block w-full outline outline-2 outline-primary-500 text-black"
 				placeholder="https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif">
 		</label>
 
@@ -61,7 +78,7 @@
 
 		<label for="description">
 		<span class="text-lg">Description</span>
-			<textarea name="og_description" id="description" class="form-textarea mt-1 block w-full outline outline-2 outline-primary-500 text-black"
+			<textarea bind:value={og_descr} name="og_description" id="description" class="form-textarea mt-1 block w-full outline outline-2 outline-primary-500 text-black"
 				placeholder="This is a real page about real things!!!"></textarea>
 		</label>
 
@@ -69,4 +86,5 @@
 			Create
 		</button>
 	</form>
+	</div>
 </div>
