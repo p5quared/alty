42a7d46e34552d5478a2e5da902bff7ed328794b
Peter Vaiciulis <petervaiciulis@gmail.com>
2024-01-10 12:43:12 -0700

    return user object (logged in)

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,10 @@
+import type { PageServerLoad } from './$types'
+
+export const load = ( async ({ locals }) => {
+	if (locals.user) {
+		return { user: locals.user }
+	} else {
+		return { user: undefined } 
+	}
+}) satisfies PageServerLoad
+
